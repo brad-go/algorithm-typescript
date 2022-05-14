6bbbbd23323ead4e3bce84cef9f0627524e2cd66
고동현 <dhjk35@naver.com>
2022-05-15 00:49:02 +0900

    Test: 테스트 설정 변경 - 상세 테스트 내역 확인 설정 추가

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
-/* eslint-disable no-undef */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.(ts|tsx)'],
   collectCoverage: true,
+  verbose: true,
 };
