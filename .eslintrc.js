1a80dbbb951b7420c6ad69747445c5ff859f6072
고동현 <dhjk35@naver.com>
2022-05-15 00:49:45 +0900

    Style: eslint 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
-// eslint-disable-next-line no-undef
 module.exports = {
   env: { es2021: true },
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 0,
+  },
 };
