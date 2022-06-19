f97b4b7886b810977048d9573dab2a97d2450c5c
고동현 <dhjk35@naver.com>
2022-06-19 15:08:10 +0900

    Modify: 버블 정렬 코드 수정

--- src/algorithms/sorting/bubble-sort/BubbleSort.ts
+++ src/algorithms/sorting/bubble-sort/BubbleSort.ts
@@ -4,11 +4,10 @@ const bubbleSort = <T>(
   array: T[],
   compareFunction: CompareFunction<T> = Comparator.defaultCompareFunction,
 ): T[] => {
-  const { length } = array;
   const comparator = new Comparator<T>(compareFunction);
 
-  for (let i = 1; i < length; i++) {
-    for (let j = 0; j < length - i; j++) {
+  for (let i = 1; i < array.length; i++) {
+    for (let j = 0; j < array.length - i; j++) {
       if (comparator.greaterThan(array[j], array[j + 1])) {
         [array[j], array[j + 1]] = [array[j + 1], array[j]];
         /* const temp = array[a];
